d451ea9978b1f4a372c98cea74fd2d4c2ad563b0
shubhrabiswal <31442710+shubhrabiswal@users.noreply.github.com>
2021-03-23 11:49:05 +0530

    added process.env.port to port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!!!!!<h1>');
